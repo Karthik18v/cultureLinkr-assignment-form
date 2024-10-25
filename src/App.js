6f0773d8e745495d8a52482093a148cc1c7f46ff
Karthik18v <bittukarthik77@gmail.com>
2024-10-25 07:52:07 +0000

    completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [showMessage, setShowMessage] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const onSubmitForm = (event) => {
+    console.log("event called");
+    event.preventDefault();
+    setShowMessage("Thank you! Your message has been sent.");
+    setName("");
+    setEmail("");
+    setMessage("");
+  };
+
+  const onChangeName = (event) => {
+    setName(event.target.value);
+  };
+
+  const onChangeEmail = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const onChangeMessage = (event) => {
+    setMessage(event.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="container" onSubmit={onSubmitForm}>
+        <h3>Contact Us</h3>
+        <div className="label-container">
+          <label htmlFor="name">Name</label>
+          <input
+            id="name"
+            type="text"
+            onChange={onChangeName}
+            value={name}
+            required
+          />
+        </div>
+        <div className="label-container">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            onChange={onChangeEmail}
+            value={email}
+            id="email"
+            required
+          />
+        </div>
+        <div className="label-container">
+          <label htmlFor="message">Message</label>
+          <textarea
+            id="message"
+            onChange={onChangeMessage}
+            value={message}
+            rows="4"
+            required
+          ></textarea>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+      <div>{showMessage}</div>
     </div>
   );
 }
